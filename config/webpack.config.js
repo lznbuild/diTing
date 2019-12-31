c09b0977831dc8ab8ba414ea81fb0c99165c6aaf
轩 <xuan@xuandeMacBook-Pro.local>
2019-12-31 14:33:50 +0800

    feat(init):add init

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,12 +1,21 @@
 const path = require("path");
  
 module.exports={
-  mode:"production",
-  entry:"./src/index.js",
-  output:{
-    path:path.resolve(__dirname,"dist"),
+    mode:"production",
+    entry:"./src/index.js",
+    output:{
+    path:path.resolve(__dirname,"../","dist"),
     filename:"feError.js",
     library:"feError",// 在全局变量中增加一个library变量
     libraryTarget:"umd"
-  }
+    },
+    module:{
+        rules:[
+            {
+                test:/.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
 }
